b0354f0c3266ee1a9ff31da09fddeb9f92bf1d27
anargiris <anargiriss@hotmail.com>
2022-08-26 00:29:25 +0300

    basic API implementation testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getPeople = async (page) => {
+    fetch(`https://randomuser.me/api/?results=3&page=${page}`)
+      .then((res) => res.json())
+      .then((data) => setPeople(data.results));
+  };
+
+  useEffect(() => {
+    getPeople();
+  }, [page]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-7xl">My Clerk App</h1>
-
-        <p className="t"></p>
-        <p></p>
+        {people && (
+          <div>
+            <br />
+            {people.map((p, i) => (
+              <div key={i}>
+                {p.name.first} {p.name.last}
+              </div>
+            ))}
+          </div>
+        )}
+        <div>
+          <button
+            type="button"
+            onClick={() => setPage(page + 1)}
+            className="bg-cyan-700 text-cyan-100 px-4 py-2 border-md my-10 text-2xl"
+          >
+            LOAD MORE PEOPLE
+          </button>
+        </div>
       </header>
     </div>
   );
